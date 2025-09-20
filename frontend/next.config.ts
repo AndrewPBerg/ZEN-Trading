f3e23609359f4f45e73c0e0e4118f725253cfa45
Andrew Berg <andberg9@gmail.com>
2025-09-20 12:19:39 -0400

    working docker compose

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:42069';
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
         // this makes the api url work with trailing slashes!
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*/`,
+        destination: `${apiUrl}/api/:path*/`,
       },
     ];
   },
   trailingSlash: true,
 };
-
 export default nextConfig;
